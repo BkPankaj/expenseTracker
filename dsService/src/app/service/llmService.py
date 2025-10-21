93294833f942bf2b00bf5a3455246f6e1e1d6982
BkPankaj <borade.keshav825@gmail.com>
2025-10-21 21:57:44 +0530

    added dsservice

---
+++ dsService/src/app/service/llmService.py
@@ -0,0 +1,36 @@
+from typing import Optional
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_google_genai import ChatGoogleGenerativeAI
+from app.service.Expense import Expense
+from dotenv import load_dotenv
+
+class LLMService:
+    def __init__(self):
+        load_dotenv()
+        self.prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are an expert extraction algorithm. "
+                "Only extract relevant information from the text. "
+                "If you do not know the value of an attribute asked to extract, "
+                "return null for the attribute's value.",
+            ),
+            ("human", "{text}")
+        ]
+        )
+        self.apiKey = os.getenv('GOOGLE_API_KEY')
+        
+        # Gemini LLM
+        self.llm = ChatGoogleGenerativeAI(
+            model="gemini-2.5-flash",
+            google_api_key=self.apiKey,
+            temperature=0
+        )
+        
+        self.runnable = self.prompt | self.llm.with_structured_output(schema=Expense)
+
+    def runLLM(self, message):
+        return self.runnable.invoke({"text":message})
