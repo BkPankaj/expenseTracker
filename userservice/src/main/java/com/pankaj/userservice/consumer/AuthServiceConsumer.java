0091b4fbd0c08d77513f068fd7f04684e432a61f
BkPankaj <borade.keshav825@gmail.com>
2025-10-22 13:19:29 +0530

    added userservice

---
+++ userservice/src/main/java/com/pankaj/userservice/consumer/AuthServiceConsumer.java
@@ -0,0 +1,32 @@
+package com.pankaj.userservice.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pankaj.userservice.entities.UserInfoDto;
+import com.pankaj.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceConsumer
+{
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void listen(UserInfoDto eventData) {
+        try{
+            userService.createOrUpdateUser(eventData);
+        }catch(Exception ex){
+            ex.printStackTrace();
+            System.out.println("AuthServiceConsumer: Exception is thrown while consuming kafka event");
+        }
+    }
+
+}
